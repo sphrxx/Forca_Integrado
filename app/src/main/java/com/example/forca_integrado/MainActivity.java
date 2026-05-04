e7d8eac14e801039b9f5ac1b417b739fe71f665a
aluno <caio.carvalho@alunos.ifsuldeminas>
2026-05-04 11:30:14 -0300

    04/05/2026

--- app/src/main/java/com/example/forca_integrado/MainActivity.java
+++ app/src/main/java/com/example/forca_integrado/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.forca_integrado;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button b;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, TelaJogo.class));
+
     }
 }
--- app/src/main/java/com/example/forca_integrado/TelaJogo.java
+++ app/src/main/java/com/example/forca_integrado/TelaJogo.java
@@ -1,6 +1,9 @@
 package com.example.forca_integrado;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaJogo extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class TelaJogo extends AppCompatActivity implements View.OnClickListener {
+    private ImageView imagem;
+    private ArrayList<Integer> listaImagens;
+    private int indiceListaImagens;
+    private Button b1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,33 @@ public class TelaJogo extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imagem = findViewById(R.id.imageView2);
+        indiceListaImagens = -1;
+        listaImagens = new ArrayList<Integer>();
+        listaImagens.add(R.drawable.forca_1_9);
+        listaImagens.add(R.drawable.forca_2_9);
+        listaImagens.add(R.drawable.forca_3_9);
+        listaImagens.add(R.drawable.forca_4_9);
+        listaImagens.add(R.drawable.forca_5_9);
+        listaImagens.add(R.drawable.forca_6_9);
+        listaImagens.add(R.drawable.forca_7_9);
+        // imagem 8 é igual à 7
+        listaImagens.add(R.drawable.forca_9_9);
+        listaImagens.add(R.drawable.forca_10_9);
+        listaImagens.add(R.drawable.forca_11_9);
+
+        b1 = findViewById(R.id.id2);
+        b1.setOnClickListener(this);
+    }
+
+    public void atualizaForca() {
+        indiceListaImagens++;
+        imagem.setImageResource(listaImagens.get(indiceListaImagens));
+    }
+
+    @Override
+    public void onClick(View v) {
+        atualizaForca();
+
     }
 }
